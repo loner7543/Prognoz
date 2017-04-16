80b9b98ecd73c6c497da3a877559d7ff68234a5e
loner7543 <Sachok1394@mail.ru>
2017-04-17 00:07:16 +0400

    cleanup

--- src/Laba2.java
+++ src/Laba2.java
@@ -6,33 +6,20 @@ import java.io.*;
 import java.util.*;
 
 public class Laba2 {
-    static double max(double data[][], int i) {
-        double max = data[0][0];
-
-        for (int j = 0; j < 130; j++)
-            if (data[j][i] > max) max = data[j][i];
-
-        return max;
-    }
-
-
-
     public static void main(String[] args) {
         double buffer;
         FileWriter fileWriter,fileWriter_test;
         String string, s[];
-        String substring;
-        double max;
         Perceptrone_model perceptrone_model=new Perceptrone_model(3,5,5,1);
 
-        double data_train[]=new double [200];
-        double data_test[]=new double [50];
-        double data_all[][]=new double[250][2];
+        double data_train[]=new double [200];// ьренировочные данные
+        double data_test[]=new double [50];// тестовые
+        double data_all[][]=new double[250][2];//все вместе
 
         int l=0;
         try {
             Scanner scanner = new Scanner(new File("Данные для прогноза.txt"));
-            Scanner Scanner_train = new Scanner(new File("train_data.txt"));
+            Scanner Scanner_train = new Scanner(new File("train_data.txt"));// и трен и тест уже нормализованы
             Scanner Scanner_test = new Scanner(new File("test_data.txt"));
             while (Scanner_train.hasNextLine()) {
                 string = Scanner_train.nextLine();
@@ -63,11 +50,12 @@ public class Laba2 {
             double dif_data[]=new double[250];
             for (int i=0;i<k;i++)
                 dif_data[i]=data_all[i][0]-data_all[i][1];
-
+//////////////////////Денормализация
             double square_sums=0;
             for (int i=0;i<k;i++)
                 square_sums+=dif_data[i]*dif_data[i];
             square_sums=Math.sqrt(square_sums);
+            ///////// Конец денормализации
 
             l=0;
 
@@ -82,12 +70,12 @@ public class Laba2 {
                 }
             }
 
-            perceptrone_model.prognos_train(data_train,data_test);
+            perceptrone_model.prognos_train(data_train,data_test);// учим
             double estimates[];
-            estimates=perceptrone_model.get_estimates(data_train,data_test);
+            estimates=perceptrone_model.get_estimates(data_train,data_test);//прогнозируем
 
           for (int i=0;i<estimates.length;i++)
-                System.out.println("Прогнозируемые данные: "+ estimates[i]*square_sums + " Реальные данные: "+data_train[i]*square_sums);
+                System.out.println("Прогнозируемые данные: "+ estimates[i]*square_sums + " Реальные данные: "+data_train[i]*square_sums);//Домножаем на квадрат тк при нормализации делили
             Scanner_train.close();
             Scanner_test.close();
 
--- src/Perceptrone_model.java
+++ src/Perceptrone_model.java
@@ -131,7 +131,7 @@ public class Perceptrone_model {
             for (int k = 0; k < output_of_layers[i].length; k++) {
                 for (int z = 0; z < output_of_layers[i + 1].length; z++)
                     buffer += errors[i + 1][z] * weights[i][z][k];
-                errors[i][k] = buffer * sigmoid_derivate(layers[i][k]);
+                errors[i][k] = buffer * sigmoid_derivate(layers[i][k]);// замена функции на произодную
                 buffer=0;
             }
 
@@ -149,10 +149,10 @@ public class Perceptrone_model {
 
     //изменение параметров сети c применением момента
     public void change_weights() {
-        for (int i = 0; i < kol_of_layers - 1; i++)
+        for (int i = 0; i < kol_of_layers - 1; i++)// перебираем слои за искл входного слоя
             for (int k = 0; k < output_of_layers[i + 1].length; k++)
                 for (int z = 0; z < output_of_layers[i].length; z++) {
-                    weights[i][k][z] += -gradient_of_weights[i][k][z] * learn_rate / (double) mini_batch + Lambda * (weights[i][k][z] - previous_weights[i][k][z]);
+                    weights[i][k][z] += -gradient_of_weights[i][k][z] * learn_rate / (double) mini_batch + Lambda * (weights[i][k][z] - previous_weights[i][k][z]);//формула корректировки ввеса
                     gradient_of_weights[i][k][z] = 0;
                     previous_weights[i][k][z] = weights[i][k][z];
                 }
@@ -250,7 +250,7 @@ public class Perceptrone_model {
 
     }
 
-    //получений оценок для тестовой выборки
+    //получений оценок для тестовой выборки(МЕТОД СКОЛЬЗЩЕГО ОКНА)// прогнозирование
     public double[] get_estimates(double data_train[],double data_test[]){
         int ind=0;
         double estimates[]=new double[layers[0].length+data_test.length];
@@ -259,20 +259,20 @@ public class Perceptrone_model {
             estimates[i]=data_train[data_train.length-5+i];
         double estimates_test[]=new double[data_test.length];
 
-        ind=0;
+        ind=0;//хранит положение начала окна
 
-        while(ind!=data_test.length){
+        while(ind!=data_test.length){// идем по выборке
             for (int j=0;j<layers[0].length;j++)
-                x[j]=estimates[j+ind];
-            Set_the_first_layer(x);
-            feed_forward();
-            estimates[ind+layers[0].length]=output_of_layers[kol_of_layers-1][0];
-            ind++;
+                x[j]=estimates[j+ind];//взяли 5 значений в окне и подали его на вход сети
+            Set_the_first_layer(x);// подали на вход и установили веса
+            feed_forward();//сделали прямое распространение
+            estimates[ind+layers[0].length]=output_of_layers[kol_of_layers-1][0];// взяли значение с соответствующего слоя и соотв нейрона в этом слое1- слойб2-нейрон
+            ind++;// сдвигаем окно на 1
         }
         for (int i=0;i<data_test.length;i++) {
             estimates_test[i] = estimates[i + layers[0].length];
         }
-        error_prognosation(estimates_test,data_test);
+        error_prognosation(estimates_test,data_test);// вычисляем ошибку на тестовых данных
         return estimates_test;
     }
 
@@ -282,11 +282,10 @@ public class Perceptrone_model {
         System.out.println("Подождите, идет процесс обучения сети!");
         double y[] = new double[layers[kol_of_layers - 1].length];
         double x[] = new double[layers[0].length];
-        double estimates[] = new double[data_test.length];
         int ind = 0;
         int m=0;
         for (int k = 0; k < number_of_epoch; k++) {
-            if (k+1%2000==0) learn_rate/=100;
+            if (k+1%2000==0) learn_rate/=100;// уменьшаем величину шага чтобы не перескочить минимум
             while (ind + layers[0].length != data_train.length) {
                 for (int j = 0; j < layers[0].length; j++)
                     x[j] = data_train[j + ind];
@@ -298,8 +297,8 @@ public class Perceptrone_model {
                         y[j] = data_train[ind + layers[0].length];
 
                 Set_the_first_layer(x);
-                feed_forward();
-                back_forward(y);
+                feed_forward();// создать сеть прямого распространения
+                back_forward(y);// создать сеть обраного распространения
                 m++;
                 if (m==mini_batch) {
                     m=0;
@@ -311,79 +310,6 @@ public class Perceptrone_model {
             ind = 0;
         }
 
-    }
-
-    //установить количество нейронов в скрытом слое
-    public void setHideLayer(int n){
-
-        Random rand=new Random();
-
-        int number_of_neurons_in_layer[]={layers[0].length,n,layers[layers.length-1].length};
-        int kol_layers=kol_of_layers;
-        layers = new double[kol_layers][];
-        kol_of_layers = kol_layers;
-        output_of_layers = new double[kol_layers][];
-        biases = new double[kol_layers][];
-        weights = new double[kol_layers - 1][][];
-        errors = new double[kol_layers][];
-        gradient_of_weights = new double[kol_layers - 1][][];
-        gradient_of_biases = new double[kol_layers][];
-
-        for (int i = 0; i < kol_layers; i++) {
-            layers[i] = new double[number_of_neurons_in_layer[i]];
-            output_of_layers[i] = new double[number_of_neurons_in_layer[i]];
-            biases[i] = new double[number_of_neurons_in_layer[i]];
-            gradient_of_biases[i] = new double[number_of_neurons_in_layer[i]];
-            errors[i] = new double[number_of_neurons_in_layer[i]];
-            for (int j = 0; j < biases[i].length; j++) {
-                if (i != 0) biases[i][j] = rand.nextGaussian();
-                else biases[i][j] = 0;
-                gradient_of_biases[i][j]=0;
-            }
-            if (i != kol_layers - 1) {
-                weights[i] = new double[number_of_neurons_in_layer[i + 1]][];
-                gradient_of_weights[i] = new double[number_of_neurons_in_layer[i + 1]][];
-                for (int j = 0; j < weights[i].length; j++) {
-                    weights[i][j] = new double[number_of_neurons_in_layer[i]];
-                    gradient_of_weights[i][j] = new double[number_of_neurons_in_layer[i]];
-                    for (int z = 0; z < number_of_neurons_in_layer[i]; z++) {
-                        weights[i][j][z] = rand.nextGaussian();
-                        gradient_of_weights[i][j][z]=0;
-                    }
-                }
-            }
-        }
-
-    }
-
-    public void write_weights_to_file() {
-
-        try {
-            FileWriter fileWriter = new FileWriter("Preceptrone_weights.txt");
-            for (int i = 0; i < kol_of_layers-1; i++) {
-                for (int j = 0; j < output_of_layers[i+1].length; j++) {
-                    for (int k = 0; k < output_of_layers[i].length; k++)
-                        fileWriter.write(String.valueOf(weights[i][j]) + " ");
-                    fileWriter.append(System.getProperty("line.separator"));
-                }
-                fileWriter.append(System.getProperty("line.separator"));
-            }
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //нахождение нейрона с наибольшим выходом
-    int find() {
-
-        int max = 0;
-
-        for (int i = 0; i < output_of_layers[kol_of_layers - 1].length; i++)
-            if (output_of_layers[kol_of_layers - 1][max] < output_of_layers[kol_of_layers - 1][i])
-                max = i;
-
-        return max;
     }
 
     public void Setnumber_of_epoch(int n){
